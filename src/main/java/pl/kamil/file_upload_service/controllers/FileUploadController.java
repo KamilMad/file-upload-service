2dcfc21597c0f623b777240b9f6c84661f712b45
KamilMad <kamilul085@gmail.com>
2025-03-19 10:33:24 +0100

    Refactor: getFile

--- src/main/java/pl/kamil/file_upload_service/controllers/FileUploadController.java
+++ src/main/java/pl/kamil/file_upload_service/controllers/FileUploadController.java
@@ -1,13 +1,17 @@
 package pl.kamil.file_upload_service.controllers;
 
+import com.amazonaws.services.s3.model.S3Object;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.kamil.file_upload_service.services.FileUploadService;
+
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/files")
@@ -23,6 +27,25 @@ public class FileUploadController {
     @PostMapping
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         String fileUrl = fileUploadService.uploadFile(file);
-        return ResponseEntity.ok(fileUrl);
+        return ResponseEntity.status(HttpStatus.OK).body(fileUrl);
+    }
+
+
+    @GetMapping("/{fileKey}")
+    public ResponseEntity<InputStreamResource> getFileById(@PathVariable String fileKey) {
+            S3Object s3Object = fileUploadService.getFile(fileKey);
+
+            if (s3Object == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+
+            InputStreamResource resource = new InputStreamResource(s3Object.getObjectContent());
+
+            // Return the byte array as the response body
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .header("Content-type", s3Object.getObjectMetadata().getContentType())
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileKey + "\"")
+                    .body(resource);
     }
 }
--- src/main/java/pl/kamil/file_upload_service/services/FileUploadService.java
+++ src/main/java/pl/kamil/file_upload_service/services/FileUploadService.java
@@ -5,12 +5,13 @@ import com.amazonaws.SdkClientException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @Service
@@ -32,11 +33,12 @@ public class FileUploadService {
 
         // Prepare metadata for the upload
         ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
         metadata.setContentLength(file.getSize());  // Set the file size in metadata
 
         try {
             // upload the file to the specific S3 bucket
-            s3Client.putObject(new PutObjectRequest(bucket, filename, file.getInputStream(), metadata));
+            s3Client.putObject(new PutObjectRequest(bucket, filename, file.getInputStream(),metadata));
             // Return the file URL from S3 bucket
             return s3Client.getUrl(bucket, filename).toString();
         } catch (AmazonServiceException e) {
@@ -49,4 +51,18 @@ public class FileUploadService {
 
     }
 
+    public S3Object getFile(String fileKey) {
+        try {
+            // Get the object from the S3 bucket using the key (file name)
+            return s3Client.getObject(new GetObjectRequest(bucket, fileKey));
+        } catch (AmazonS3Exception e) {
+            if (e.getStatusCode() == 404) {
+                throw new FileNotFoundException("File not found: " + fileKey);
+            }
+            throw new S3StorageException("S3 error retrieving file: " + fileKey, e);
+        } catch (SdkClientException e) {
+            throw new StorageServiceException("AWS SDK client error", e);
+        }
+
+    }
 }
