19b4477c1392dcf2410fe1b56a2bc55dad420f45
KamilMad <kamilul085@gmail.com>
2025-03-19 14:53:35 +0100

    Refactor: change S3FileResponse to record

--- src/main/java/pl/kamil/file_upload_service/dtos/S3FileResponse.java
+++ src/main/java/pl/kamil/file_upload_service/dtos/S3FileResponse.java
@@ -3,6 +3,8 @@ package pl.kamil.file_upload_service.dtos;
 import org.springframework.core.io.InputStreamResource;
 
 
-public record S3FileResponse(InputStreamResource inputStreamResource, String contentType) {
+public record S3FileResponse(
+        InputStreamResource inputStreamResource,
+        String contentType) {
 
 }
