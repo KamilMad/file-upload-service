8d8bfef90e2e1a680328115a5c79a15f85ab070d
KamilMad <kamilul085@gmail.com>
2025-06-11 20:56:28 +0200

    MODIFY; version of aws sdk to 2

---
+++ src/main/java/pl/kamil/file_upload_service/utilities/ExceptionMapper.java
@@ -0,0 +1,33 @@
+package pl.kamil.file_upload_service.utilities;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+public class ExceptionMapper {
+
+    private ExceptionMapper() {
+
+    }
+
+    public static ResponseStatusException mapS3DeleteException(Exception e, String key) {
+        if (e instanceof NoSuchKeyException){
+            return new ResponseStatusException(HttpStatus.NOT_FOUND, "File not found: " + key, e);
+        } else if (e instanceof S3Exception s3Exception) {
+            int statusCode = s3Exception.statusCode();
+
+            if (statusCode == 403) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access denied to file: " + key, e);
+            }
+
+        } else if (e instanceof SdkClientException){
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "AWS SDK client error: " + e.getMessage(), e);
+        } else if(e instanceof AwsServiceException awse){
+            throw new ResponseStatusException(HttpStatus.BAD_GATEWAY, "AWS service error: " + awse.awsErrorDetails().errorMessage(), e);
+        }
+        return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error deleting file: " + key, e);
+    }
+}
